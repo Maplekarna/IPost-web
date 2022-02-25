b3bc76cd7b3275215f9aaef60361f04f037c71e3
Shuai <maplekarna@gmail.com>
2022-02-25 12:01:08 -0800

    Feb 25, 2022

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root')  // Control DOM
 );
 
 // If you want to start measuring performance in your app, pass a function
